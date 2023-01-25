8f1da3198a5bf872f95bcf6db0844f94278d49fc
fawnbradshaw <fbradsha@chicagobooth.edu>
2023-01-25 07:43:50 +0000

    Omnicalc: square root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,33 @@ def calculate_square
 
 end
 
+
+def square_root_form
+
+  render({ :template => "calculation_templates/square_root_form.html.erb"})
+  
+end
+
+def calculate_square_root
+  @num = params.fetch("user_number").to_f
+  @square_root_of_num = @num ** (1/2.0)
+
+  render({ :template => "calculation_templates/square_root_results.html.erb"})
+
+end
+
+
+def random_form
+  render({ :template => "calculation_templates/random_form.html.erb"})
+end
+
+
+def calculate_random
+  @lower = params.fetch("user_min").to_f
+  @upper = params.fetch("user_max").to_f
+  @rand_num = rand(@lower..@upper)
+  render({ :template => "calculation_templates/rand_results.html.erb"})
+
+end
+
 end
